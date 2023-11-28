8ec09df098cfe740c06d2cf35702f9c2f1702046
Ricardo Coelho <1191446@isep.ipp.pt>
2023-11-28 22:58:46 +0000

    [LAB-115][Source Code][Console application Display]

---
+++ display_mcs_fo/src/main/java/labdsoft/display_mcs_fo/DisplayMcsFOApplication.java
@@ -0,0 +1,13 @@
+package labdsoft.display_mcs_fo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplayMcsFOApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisplayMcsFOApplication.class, args);
+    }
+
+}
